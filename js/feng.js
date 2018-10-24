73c3aec4f7ba7588c6bcc22228fa5d1443b975d0
fjuchenghen <fjuchenghen@gmail.com>
2018-10-24 19:59:13 +0800

    豐原車站重新上傳

---
+++ js/feng.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $(".menu").on("click",function(){
+        $("#Menu").show();
+    })
+
+    $("#close_btn").on("click",function(){
+        $("#Menu").hide();
+            
+    })
+        
+
+
+});
